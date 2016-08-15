6fa8ae518fb12e0002314e65da2afac13af5fe19
Shiyi Chen <sychen@udel.edu>
2016-08-15 17:07:09 -0400

    null

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+import java.util.Queue;
 
 public class BST<Key extends Comparable<Key>, Value> {
 	private Node root;
@@ -11,8 +13,19 @@ public class BST<Key extends Comparable<Key>, Value> {
 			this.val = val;
 		}
 	}
-	public void put(Key key, Value value) {
-		
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+	}
+	public Node put(Node x, Key key, Value val) {
+		if (x==null) return new Node(key,val);
+		int cmp = key.compareTo(x.key);
+		if 		(cmp<0)
+			x.left = put(x.left, key, val);
+		else if (cmp>0)
+			x.right = put(x.right, key, val);
+		else
+			x.val = val;
+		return x;
 	}
 	public Value get(Key key) {
 		Node x = root;
@@ -27,7 +40,29 @@ public class BST<Key extends Comparable<Key>, Value> {
 	public void delete(Key key) {
 		
 	}
+	public Key floor(Key key) {
+		Node x = floor(root, key);
+		if (x==null) return null;
+		return x.key;
+	}
+	public Node floor(Node x, Key key) {
+		if (x==null) return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp==0) return x;
+		if (cmp<0) return floor(x.left, key);
+		Node t = floor(x.right, key);
+		if (t!=null) return t;
+		else return x;
+	}
 	public Iterator<Key> iterator() {
-		
+		Queue<Key> q = new Queue<Key>();
+		inorder(root, q);
+		return q;
+	}
+	private void inorder(Node x, Queue<Key> q) {
+		if (x==null) return;
+		inorder(x.left, q);
+		q.enqueue(x.key);
+		inorder(x.right, q);
 	}
 }
