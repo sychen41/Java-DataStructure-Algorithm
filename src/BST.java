fe5c2e07f9d18838b4c753c7ae92b7606d847bd1
Shiyi Chen <sychen@udel.edu>
2016-08-15 09:38:16 -0400

    1st

--- src/BST.java
+++ src/BST.java
@@ -2,10 +2,32 @@
 public class BST<Key extends Comparable<Key>, Value> {
 	private Node root;
 	private class Node {
-		//c3
-		//c
-		//c5
-		//c4
-
+		private Key key;
+		private Value val;
+		private Node left, right;
+		
+		public Node(Key key, Value val) {
+			this.key = key;
+			this.val = val;
+		}
+	}
+	public void put(Key key, Value value) {
+		
+	}
+	public Value get(Key key) {
+		Node x = root;
+		while (x!=null) {
+			int cmp = key.compareTo(x.key);
+			if (cmp>0) x = x.right;
+			else if (cmp<0) x = x.left;
+			else return x.val;
+		}
+		return null;
+	}
+	public void delete(Key key) {
+		
+	}
+	public Iterator<Key> iterator() {
+		
 	}
 }
